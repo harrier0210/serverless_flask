1a0d26a34fca2119461e6eb5c23d998ae2c5c24b
harrier0210 <k.hariya210@gmail.com>
2019-09-19 16:37:43 +0900

    update:app.py

--- main.py
+++ main.py
@@ -2,9 +2,39 @@ from flask import Flask
 
 app = Flask(__name__)
 
-@app.route("/")
-def hello():
-    return "Hello World!"
+@app.route('/sign_in', methods=['post'])
+#@fresh_jwt_required
+def login():
+    try:
+        name = request.json.get("name")
+        password = request.json.get("password")
+        sql = "SELECT id, name, password FROM users WHERE BINARY name=%s"
+        db = DB
+        user = db.execute(sql, [ name ])
+        if not (user and bcrypt.checkpw(password.encode(), user['password'].encode())):
+             return jsonify( {"message": "ユーザー名又はパスワードが違います。"} ), 401
+    except Exception as e:
+        traceback.print_exc()
+        return jsonify( {"message": "An error occurred"} ), 500
 
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=3000, debug=True)
+    access_token = create_access_token(identity=user["id"], fresh=True)
+    sql = "UPDATE users SET jti=%s WHERE name=%s"
+    DB.update(sql, [ get_jti(access_token), 'python' ])
+    sql = "SELECT name, jti FROM users"
+    user = DB.execute_list(sql)
+    return jsonify(access_token=access_token), 200
+
+@app.route("/protected", methods=["GET"])
+@fresh_jwt_required
+def protected():
+    user = auth_jti(get_jwt_identity(), get_raw_jwt()["jti"])
+    if not user:
+        return jsonify( {"message": "Bad access token"} ), 401
+    return jsonify( user ), 200
+
+def auth_jti(id, token_jti):
+    sql = "SELECT id, name, jti FROM users WHERE id=%s"
+    user = DB.execute(sql, [ id ])
+    if token_jti == user["jti"]:
+        return user
+    return False
